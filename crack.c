4c697184a9f99f0472a0a6c9b0dc2b1a85f5e972
Barry Brown <profbbrown@gmail.com>
2020-11-19 17:26:59 +0000

    Example of bsearch

--- crack.c
+++ crack.c
@@ -4,8 +4,7 @@
 
 #include "md5.h"
 
-const int PASS_LEN=50;        // Maximum any password can be
-const int HASH_LEN=33;        // Length of MD5 hash strings
+const int PASS_LEN=50;        // Maximum any password will be
 
 
 // Stucture to hold both a plaintext password and a hash.
@@ -17,7 +16,7 @@ struct entry
 
 
 // TODO
-// Read in the dictionary file and return an array of structs.
+// Read in the dictionary file and return an array of entry structs.
 // Each entry should contain both the hash and the dictionary
 // word.
 struct entry *read_dictionary(char *filename, int *size)
@@ -39,8 +38,14 @@ int main(int argc, char *argv[])
     struct entry *dict = read_dictionary(NULL, NULL);
     
     // TODO: Sort the hashed dictionary using qsort.
-    // You will need to provide a comparison function.
+    // You will need to provide a comparison function that
+    // sorts the array by hash value.
     qsort(dict, 0, 0, NULL);
+    
+    // Partial example of using bsearch, for testing. Delete this line
+    // once you get the remainder of the program working.
+    // This is the hash for "rockyou".
+    struct entry *found = bsearch("f806fc5a2a0d5ba2471600758452799c", dict, 0, 0, NULL);
 
     // TODO
     // Open the hash file for reading.
